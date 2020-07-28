e2c167bc1460a50cb964168d2d110682b99ffccf
Jonathan Li <jonathan.li.1997@gmail.com>
2020-07-28 09:58:30 -0400

    updated TwoPointers

--- Alogrithms/TwoPointer.py
+++ Alogrithms/TwoPointer.py
@@ -1,11 +1,6 @@
 ''' Container with Most Water '''
 
-def maxArea(self, height):
-    """
-    :type height: List[int]
-    :rtype: int
-    """
-    
+def maxArea(height):
     max_area = 0
     
     left = 0                    #Two pointers, one starts at 0, other starts at end
@@ -21,3 +16,10 @@ def maxArea(self, height):
             right = right - 1
     
     return max_area
+
+def main():
+    height = [1,8,6,2,5,4,8,3,7]
+    print("Result of maxArea():  " + str(maxArea(height)))
+
+if __name__ == "__main__":
+    main()
