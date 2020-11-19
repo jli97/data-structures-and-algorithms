4dec1a61a6962c1b1468c8f9abe72f928542a9dd
Jonathan Li <jonathan.li.1997@gmail.com>
2020-11-19 10:29:21 -0500

    updated algorithms

---
+++ Alogrithms/Recursion/ConcatenatedWords.py
@@ -0,0 +1,65 @@
+class TrieNode:
+    def __init__(self):
+        self.isWord = False
+        self.children = {}
+    
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def addWord(self, word):
+        cur = self.root
+        
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            
+            cur = cur.children[c]
+        
+        cur.isWord = True
+    def searchWord(self, word): 
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isWord       
+        
+''' Doesn't account for edge case ["a", "aa", "aaa"..."a*n"]'''
+
+class Solution:
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        
+        t = Trie()
+        
+        for word in words:
+            t.addWord(word)
+        
+        def search(word, idx, node, count, ret):
+            if idx == len(word):
+                if (node.isWord or node == t.root) and count > 1 and word not in ret:
+                    ret.add(word)
+            
+                return
+                
+            char = word[idx]
+            
+            if char in node.children:
+                node = node.children[char]
+                if node.isWord: # Two options use or not use this as a word
+                    use = search(word, idx + 1, t.root, count + 1, ret) # Increment count, and start at root again
+                    notUse = search(word, idx + 1, node, count, ret) 
+                else:
+                    search(word, idx + 1, node, count, ret)
+            else:
+                return
+            
+            
+            
+            
+        ret = set()   
+        for word in words:
+            if word:
+                search(word, 0, t.root, 0, ret)
+            
+        return list(ret)
