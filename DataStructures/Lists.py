fd2fdc503f62599cdd76ae22e056f71ed75f4d40
Jonathan Li <jonathan.li.1997@gmail.com>
2020-11-29 17:30:20 -0500

    updated datastructures

--- DataStructures/Lists.py
+++ DataStructures/Lists.py
@@ -16,6 +16,24 @@ def createLinkedList(arr: list, head: ListNode): #Array to LinkedList
 
         When you reference head, you lose the reference of your previous node
         '''
+def reverseListRecursive(self, head: ListNode) -> ListNode:
+    if head is None or head.next is None: 
+        return head
+    
+    p = self.reverseList(head.next) #Head node of reversed list
+    head.next.next = head # After the first recurisve call ends, head is the 
+    head.next = None      # second last node due to the base case above
+    return p
+
+def reverseListIteratively(self, head: ListNode) -> ListNode:
+    prev = None
+    while head:
+        next = head.next
+        head.next = prev
+        prev = head
+        head = next
+                
+    return prev
 
 def printLinkedList(head: ListNode):
     ret = []
