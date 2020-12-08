cc16e2db7b7b591fcfac93dfb12be2b0084e2112
Jonathan Li <jonathan.li.1997@gmail.com>
2020-12-08 08:51:07 -0500

    updated algorithms

--- Alogrithms/Greedy/JumpGameII.py
+++ Alogrithms/Greedy/JumpGameII.py
@@ -3,8 +3,6 @@
     the maximum end of all indexes within the interval of index 0. Jump
     to the max index, and repeat the search for the farthest endpoint 
     within the new interval. 
-
-
 '''
 
 def jump(nums):
@@ -12,18 +10,19 @@ def jump(nums):
         intervals = [(i, i + nums[i]) for i in range(len(nums))]
 
         ans = 0
-        j = 0
+        j = 0 # Used to iterate over the other intervals interval[start] can jump to
         start, end = 0, 0
     
         while start < len(nums) - 1:
             ans += 1
-            r = end
-            
-            while j < len(nums) and intervals[j][0] <= r:
-                end = max(end, intervals[j][1])
+            r = end # Temp var for max distance interval[start] can reach
+                    
+            # For every interval[start][0] can reach
+            while j < len(nums) and intervals[j][0] <= r: 
+                end = max(end, intervals[j][1]) # Pick the farthest reaching interval    
                 j += 1
             
-            start = end
+            start = end # Repeat process for new start index
         
         return ans
 
